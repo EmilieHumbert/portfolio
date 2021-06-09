919d8f46307580115ce9cd34b950db542a122b28
emilie <emi.m.humbert@googlemail.com>
2021-06-09 17:06:56 +0200

    Add intro to homepage

---
+++ components/intro.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div>
+      <h1 className="mt-12 mb-8 lg:mt-24 lg:mb-12 text-4xl lg:text-7xl">
+        Grüetzi,
+        <br /> I'm Emilie
+      </h1>
+      <p className="text-xl lg:text-3xl leading-relaxed lg:leading-loose">
+        A front-end developer based in Zürich, Switzerland.
+        <br /> I build responsive websites with Next.js
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Navigation from "../components/navigation";
+import Intro from "../components/intro";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <div className="w-full sm:w-3/5">
+    <div className="flex justify-center mx-2">
+      <div className="border-b-2 border-gray-300 pb-24 mt-6 w-full sm:w-3/5">
         <Navigation />
+        <Intro />
       </div>
     </div>
   );
