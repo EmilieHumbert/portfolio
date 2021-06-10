41e9f2d3b237071a4a43198a697bfd6dda170da5
emilie <emi.m.humbert@googlemail.com>
2021-06-10 14:12:37 +0200

    Add projects to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import React from "react";
 
 import Navigation from "../components/navigation";
 import Intro from "../components/intro";
+import Projects from "../components/projects";
 
 export default function Home() {
   return (
     <div className="flex justify-center mx-2">
-      <div className="border-b-2 border-gray-300 pb-24 mt-6 w-full sm:w-3/5">
+      <div className="border-b-2 border-gray-300 pb-24 mt-6 w-full sm:w-4/5">
         <Navigation />
-        <Intro />
+        <div className="border-b-2 border-gray-300">
+          <Intro />
+        </div>
+        <div className="border-b-2 border-gray-300">
+          <Projects />
+        </div>
       </div>
     </div>
   );
