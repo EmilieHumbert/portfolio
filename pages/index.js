5deb9b92aef06d3c72f84353242bd59d777677d9
emilie <emi.m.humbert@googlemail.com>
2021-06-10 17:42:43 +0200

    Add themed background colour

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ import Projects from "../components/projects";
 
 export default function Home() {
   return (
-    <div className="flex justify-center mx-2">
-      <div className="pb-24 mt-6 w-full sm:w-4/5">
-          <Navigation />
+    <div className="bg-gray-background flex justify-center mx-2">
         <div className="border-b-2 border-gray-300">
           <Intro />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          background: "#fafafa",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
