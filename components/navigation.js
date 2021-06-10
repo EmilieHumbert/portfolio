5dd0a9ae130194362765a03589ee78ec51290b00
emilie <emi.m.humbert@googlemail.com>
2021-06-10 14:42:53 +0200

    Add links to sections and modify CSS

--- components/navigation.js
+++ components/navigation.js
@@ -1,48 +1,54 @@
 import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const getHref = (page) => (page === "home" ? "/" : `#${page}`);
 
 export default function Navigation() {
-  const pages = ["Home", "About", "Projects", "Contact"];
+  const router = useRouter();
+  const pages = ["about", "projects", "contact"];
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="flex flex-wrap content-center h-auto justify-between mt-5">
-      <div className="flex flex-wrap content-center">
-        <img alt="logo" src="/images/logo.png" className="w-12 lg:w-20" />
-      </div>
-      {pages.map((page, index) => {
+    <div className="border-b-4 border-gray-300 flex flex-wrap h-24 justify-between mt-5 pb-5">
+      <Link href="/">
+        <div className="content-center flex flex-wrap">
+          <img alt="logo" src="/images/logo.png" className="w-12 lg:w-20" />
+        </div>
+      </Link>
+      {pages.map((page) => {
         return (
-          <div>
-            <a
-              key={index}
-              className="hidden lg:flex lg:flex-wrap lg:content-center gap-x-8 h-0 lg:h-full"
-            >
+          <Link key={page} href={getHref(page)}>
+            <a className="hidden lg:flex lg:flex-wrap lg:content-center gap-x-8 h-0 lg:h-full">
               {page}
             </a>
-          </div>
+          </Link>
         );
       })}
 
-      <div className="flex flex-col content-center justify-between lg:hidden">
+      <div className="lg:hidden content-center flex flex-col justify-between">
         <div>
           <button
             onClick={() => setIsActive(!isActive)}
-            className="focus:outline-none px-4 mt-2 content-center uppercase hover:text-gray-500"
+            className="content-center focus:outline-none px-4 mt-2 hover:text-gray-500 uppercase"
           >
             Menu
           </button>
-          <div className="absolute bg-white flex flex-col left-2/3 right-0 mt-1 w-auto ">
+          <div className="absolute bg-white flex flex-col left-2/3 mt-1 right-0 w-auto">
             {isActive &&
               pages.map((page) => {
                 return (
-                  <a
-                    key={page}
-                    className="bg-white bottom-0 capitalize focus:outline-none pt-4 pl-4 pr-4 hover:text-gray-500 cursor-pointer"
-                    onClick={() => {
-                      setIsActive(!isActive);
-                    }}
-                  >
-                    {page}
-                  </a>
+                  <Link key={page} href={getHref(page)}>
+                    <a
+                      key={page}
+                      className="bg-white bottom-0 capitalize cursor-pointer focus:outline-none hover:text-gray-500 pt-4 pl-4 pr-4"
+                      onClick={() => {
+                        setIsActive(!isActive);
+                      }}
+                    >
+                      {page}
+                    </a>
+                  </Link>
                 );
               })}
           </div>
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ import Projects from "../components/projects";
 export default function Home() {
   return (
     <div className="flex justify-center mx-2">
-      <div className="border-b-2 border-gray-300 pb-24 mt-6 w-full sm:w-4/5">
-        <Navigation />
+      <div className="pb-24 mt-6 w-full sm:w-4/5">
+          <Navigation />
         <div className="border-b-2 border-gray-300">
           <Intro />
         </div>
-        <div className="border-b-2 border-gray-300">
+        <div className="border-b-2 border-gray-300" id="projects">
           <Projects />
         </div>
       </div>
