bc04fe34d98b54bb9bb6aa15829d379d5921b744
emilie <emi.m.humbert@googlemail.com>
2021-12-02 16:46:29 +0100

    Add dots in the project section

--- components/projects.js
+++ components/projects.js
@@ -5,18 +5,35 @@ import Project from "./project";
 export default function Projects() {
   const projects = [
     {
+      title: "Dots",
+      imageFirst: "/images/dots.png",
+      imageDescription: "Dots quiz page",
+      decription: "A progressive web app connecting vulnerable people in Senegal with organisations that can help them. This was a team project as part of deploy(impact) and organised by Women++. In this project, I was the frontend developer and worked closely with the UX researchers for the design.",
+      tools: [
+        ["Framework", "React"],
+        ["Responsive", "yes"],
+
+        ["Contact form", "react-hook-form"],
+        ["Styling", "Tailwind"],
+        ["Internationalisation", "react-i18next"],
+        ["Deployment", "Microsoft Azure"],
+      ],
+      website: "https://dots.azureedge.net",
+      github: "https://github.com/WomenPlusPlus/deploy-impact-21-kona-b",
+    },
+    {
       title: "Villa les Pieds Rouges",
       imageFirst: "/images/villa-les-pieds-rouges-homepage.png",
       imageDescription: "Villa les Pieds Rouges homepage",
       decription:
-        "Responsive bilingual website presenting luxurious holiday home in south of France. This Next.js website has multiple pages: home, gallery, plans (to view a plan and description for each floor), about, location and contact. The contact form was made using react-hook-form, axios and Sendgrid and the website was deployed to Vercel",
+        "Responsive bilingual website presenting luxurious holiday home in south of France. This Next.js website has multiple pages: home, gallery, plans (to view a plan and description for each floor), about, location and contact. The contact form was made using react-hook-form, axios and Sendgrid and the website was deployed to Vercel.",
       tools: [
         ["Framework", "Next.js"],
-        ["Deployment", "Vercel"],
+        ["Responsive", "yes"],
         ["Contact form", "Sendgrid"],
         ["Styling", "Tailwind"],
-        ["Internationalisation", "react-i18n"],
-        ["Responsive", "yes"],
+        ["Internationalisation", "next-i18next"],
+        ["Deployment", "Vercel"],
       ],
       website: "https://villa-les-pieds-rouges.vercel.app/en",
       github: "https://github.com/EmilieHumbert/villa-les-pieds-rouges",
@@ -29,11 +46,11 @@ export default function Projects() {
         "Instagram clone built with Next.js, Firebase and Tailwind CSS. This React project has multiple pages: login, sign up, dashboard (to view/like/comment on photos), and user profiles. The sign-in page connects to Firebase when a user tries to sign in, and when a user signs up. Firebase firestore handles all the data, and that data is retrieved using a custom hook. After this application was built, it was deployed to Vercel. Lighthouse was also used to improve the website performance.",
       tools: [
         ["Framework", "Next.js"],
-        ["Deployment", "Vercel"],
+        ["Responsive", "yes"],
         ["Storage", "Firestore"],
         ["Styling", "Tailwind"],
         ["Authentification", "Firebase"],
-        ["Responsive", "yes"],
+        ["Deployment", "Vercel"],
       ],
       website: "https://react-instaclone.vercel.app/login",
       github: "https://github.com/EmilieHumbert/instagram",
